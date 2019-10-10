56b4fda8f3060e9f93b01001cb4b0d8bae030f3a
denisrodin <denis.for.logs@gmail.com>
2019-10-10 20:32:49 +0300

    добавлен вывод строки Lesson3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,5 +12,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world");
+
+        System.out.println("Lesson3");
     }
 }
