e6aaad418d800da17c5c66a2127fb2b890c4cd99
denisrodin <denis.for.logs@gmail.com>
2019-10-13 16:24:18 +0300

    добавлена проверка метода setCount для класса Task в тест TaskTest

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -2,15 +2,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TaskTest {
 
+    private Task task;
+
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
-    }
-
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
+        //перед каждым тестом создаем новый объект класса Task
+        //старый объект класса Task будет удален GG по наступлению соответствующего трешхолда
+        task = new Task();
     }
 
     @org.junit.jupiter.api.Test
     void setCount() {
+        task.setCount(5);
+
+        assertEquals(5, task.getCount());
     }
 }
