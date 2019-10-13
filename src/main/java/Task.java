a6035372083a2eb1a8e5e67a1f66b54466c1de1f
denisrodin <denis.for.logs@gmail.com>
2019-10-13 15:56:19 +0300

    добавлен класс Task

---
+++ src/main/java/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
